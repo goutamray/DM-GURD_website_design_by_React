1bf432a0061e2f148afb6c6ce4ff135a4f6dd86b
goutam <goutamr362@gmail.com>
2024-05-29 21:40:15 +0600

    About page & replace page done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,43 @@
+import logo from "../../assets/DM-GURD.png"
 
-
-
+import "./About.css"; 
 const About = () => {
   return (
     <>
-      <h2> About page </h2>
+      <div className="about">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="head-title"> 
+                <h3 className="text-center"> About Us </h3>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="photo">
+                <img src={logo} alt="" />
+              </div>
+            </div>
+            <div className="col-md-6">
+               <div className="about-content">
+                <h2> DEAR CUSTOMERS </h2>
+                <h5> We Are Working since 2016 to till now with hour thousand satisfied clients from the world. Our main priority is product quality and availability. Based on more than two years of experience in affiliate marketing, we carefully select and test FB accounts for advertising Facebook Ad and Facebook BMs. We also try to maintain a low cost in the accounts market. You can buy from us: facebook warmed up accounts, Facebook Softreg PVA Accounts, Facebook Softreg PVA Accounts with BM, Business Manager Facebook 250 $, Verified & Unlimited FB BM, Softreg tiktok ads, etc. </h5>
+                <h4> - DMGurd </h4>
+               </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="thank-you">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h2 className="text-center"> Thank You So Much... </h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/replace/Replace.jsx
+++ src/pages/replace/Replace.jsx
@@ -1,9 +1,49 @@
 
 
+
+import "./Replace.css"
 const Replace = () => {
   return (
     <>
-      <h2> Replace Rules </h2>
+      <div className="replace-rules">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="breadcrumb">
+                <h2 className="text-white"> REPLACE RULES </h2>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+
+      <div className="important-content"> 
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h4> Very important information: </h4>
+              <div className="replace-content shadow p-5">
+                <p> 1. Guarantee for accounts 30 minutes  (In an hour, two, a day, do not write about the replacement, please)  guarantee for bm 1 day. </p>
+                <p> 2. Do not check purchased accounts with public checkers, and do not log in from low-quality proxies. </p>
+                <p> 3. Accepted item is non-refundable, replacement accounts only. </p>
+                <p> 4. Buy the amount that you can use in the near future. </p>
+                <p> 5. We do not give advice on working with FB, buy if you know how to use them. </p>
+                <p> 6. Blocking after doing something is not a reason to replace. </p>
+                <p> 7. Your 100% replacement guarantee – video recording. </p>
+                <p> 8. If the mail in the set is blocked, this is not a reason to replace the account. </p>
+                <p> 9. If you linked a card in billing and got blocked, this is not a reason to change your account. But you can write to support and if there is an up-to-date solution, we will show you how to fix it. </p>
+                <p> 10. If there is a ban on advertising activities on the account, this is not a reason to replace the account (except for accounts that have passed the ban on advertising activities). </p>
+                <p> 11. If after creating a fan page, or business manager, your account was blocked, this is not a reason for replacement. (Often this is the trigger for an account on new hardware). We advise you to create this after resting in your anti-detect. </p>
+                <p> 12. If it is not possible to create a business manager on a Facebook account, this is not a reason to change the account. If you need accounts with BMs – buy ready-made ones. </p>
+                <p> 13. If you have linked a business manager to your account and for some reason social. the network does not allow you to fully work with the business manager (link advertising cabinets there, etc.) – this is not a reason to change your account. But you can write to support and if there is an up-to-date solution, we will show you how to fix it. </p>
+                <p> 14. If you bought an account with a token, uploaded it to auto-bay, and found that there is no advertising account there, this is not a reason to change your account. (This is easily treated by entering ADS, this does not happen often, but unfortunately, such a bug happens). </p>
+                <p className="text-class"> Please note that when you contact support for a replacement, the store has the right to require a video of the purchase. Recording a video is an optional procedure, but this is your 100% guarantee to receive a replacement in case of a failure. When recording a video of buying an account, you need to record the moment of payment and the issuance of goods from the store, and then the subsequent verification of the account for validity through the service  https://checkaccs.nppr.team/ . Copy only by clicking the right mouse button, i.e. videos where data was copied via CTRl+C are not accepted. PS For those who are especially smart: you do not need to record a video after purchase. You need to record the moment of payment and validation as indicated below. Exactly in that order. </p>
+              </div>
+            </div>
+          </div>
+                
+          </div>
+      </div>
     </>
   )
 }
