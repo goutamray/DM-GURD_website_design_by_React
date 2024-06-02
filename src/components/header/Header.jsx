2c93d1c055d945f6d3c2589a4c58d95b3a87a939
goutam <goutamr362@gmail.com>
2024-06-03 01:06:00 +0600

    header updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,60 +1,61 @@
 import { Link } from "react-router-dom"
 import logo from "../../assets/DM-GURD.png"
-import { FaCartArrowDown } from "react-icons/fa";
-import { IoIosSend } from "react-icons/io";
-import { FiMenu } from "react-icons/fi";
+ import { FaCartArrowDown } from "react-icons/fa";
+ import { IoIosSend } from "react-icons/io";
 
 
-import "./Header.css"
+
+ import "./Header.css"
 const Header = () => {
   return (
     <>
-     <div className="header">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2 part1">
-             <div className="logo">
-              <Link to="/"> 
-                 <img src={logo} alt="" />
-              </Link>
-             </div>
-          </div>
-          <div className="col-md-10 part2">
-              <input className="check-box" type="checkbox"/>
-              <span > <FiMenu /> </span>
-           
-          
-              <ul>
-                <li>
-                  <Link to="/"> Home </Link>
-                </li>
-                <li>
-                  <Link to="/about-us"> About Us </Link>
-                </li>
-                <li>
-                  <Link to="/replace-rule"> Replace Rules </Link>
-                </li>
-              
-                <li>
-                  <Link to="/contact"> Contact </Link>
-                </li>
-                <li className="my-btn">
-                  <Link to="/my-account" className="order-btn"> my order <span> <FaCartArrowDown /> </span> </Link>
-                </li>
-                <li>
-                <Link to="" className="support-btn"> Support <span> <IoIosSend /> </span> </Link>
-                </li>
+    <nav className="navbar navbar-expand-lg navbar-light ">
+      <div className="container my-container ">
+         {/* logo part  */}
+        <Link className="navbar-brand" href="#">
+            <img src={logo} alt=""/> 
+        </Link>
 
-              </ul>
-        
-          </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+         {/* menu part  */}
+        <div className="collapse navbar-collapse my-custom " id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+
+            <li className="nav-item me-2">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+
+            <li className="nav-item me-2">
+              <Link className="nav-link" to="/about-us">About us</Link>
+            </li>
+
+            <li className="nav-item me-2">
+              <Link className="nav-link" to="/replace-rule"> Replace Rules </Link>
+            </li>
+
+            <li className="nav-item me-2">
+              <Link className="nav-link" to="/contact"> Contact </Link>
+            </li>
+
+            <li className="nav-item me-2 my-btn">
+                  <Link to="/my-account" className="nav-link"> my order <span> <FaCartArrowDown /> </span> </Link>        
+            </li>
+            <li className="nav-item me-2">
+               <Link to="/" className="nav-link support-btn"> Support <span> <IoIosSend /> </span> </Link>
+            </li>
+
+          </ul>
+
         </div>
       </div>
-     </div>
+      </nav>
+
     </>
   )
 }
 
-export default Header
+export default Header;
 
 
